1541cacd8a53c26ee754927e58cfee68c0232432
紹瑋 藍 <maimaisatt@gmail.com>
2023-07-18 12:58:54 +0800

    取得編碼

--- MyFuture/BackgroundServices/GetJumpEmptyStocks.cs
+++ MyFuture/BackgroundServices/GetJumpEmptyStocks.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Extensions.Caching.Memory;
+using MyFuture.Models;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace MyFuture.BackgroundServices
 {
@@ -11,21 +14,61 @@ namespace MyFuture.BackgroundServices
         }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            await Process();   
+            try
+            {
+                List<string> stockList = GetStockList();
+                if (!_memoryCache.TryGetValue($"{DateTime.Now.Date}_2330", out string value))
+                {
+                    await SetStockInfoCache();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            
+           
+            //await Task.Delay(1000 * 60);
         }
-        private async Task Process()
+        private async Task<List<StockDetailModel>> SetStockInfoCache()
         {
             HttpClient client = new HttpClient();
             string url = "https://tw.quote.finance.yahoo.net/quote/q?type=ta&perd=d&mkt=10&sym=2330&v=1&callback=jQuery111306311117094962886_1574862886629&_=1574862886630";
             var responseMsg = await client.GetAsync(url);
-            string? result = null;
+            string? data = null;
+            List<StockDetailModel> result = new List<StockDetailModel>();
             string cacheKey = $"{DateTime.Now.Date}_2330"; 
             if (responseMsg.IsSuccessStatusCode)
             {
-                result = await responseMsg.Content.ReadAsStringAsync();
+                data = await responseMsg.Content.ReadAsStringAsync();
+                data = data.Split("\"ta\":")[1].Split(",\"ex\"")[0];
             }
+            result = JsonSerializer.Deserialize<List<StockDetailModel>>(data);
             _memoryCache.Set(cacheKey, result, TimeSpan.FromMinutes(1));
+            return result;
         }
-
+        private List<string> GetStockList()
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory.ToString(), "IPO&OTC.csv");
+            if (!File.Exists(path))
+            {
+                throw new Exception("Cannot find IPO&OTC.csv");
+            }
+            var reader = new StreamReader(File.OpenRead(path));
+            List<string> result = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                foreach (var i in values)
+                {
+                    if (int.TryParse(i, out int a))
+                    {
+                        result.Add(i);
+                    }
+                }
+            }
+            return result;
+        }
     }
 }
---
+++ MyFuture/Models/StockDetailModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFuture.Models
+{
+    public class StockDetailModel
+    {
+        public int t {  get; set; }
+        public double o { get; set; }
+        public double h { get; set; }
+        public double l { get; set; }
+        public double c { get; set; }
+        public int v { get; set; }
+    }
+}
