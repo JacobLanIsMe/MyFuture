2ded70af172265bcf7db3e1dbc19b13fe5db9770
Jacob <maimaisatt@gmail.com>
2023-09-18 12:10:07 +0800

    類別庫加到 sln

--- MongoFactory/Factorys/Factory.cs
+++ MongoFactory/Factorys/Factory.cs
@@ -2,22 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
 
 namespace MongoFactory.Factorys
 {
     public class Factory
     {
         private string connString = null;
-        private 
         public Factory(IConfiguration config)
         {
             connString = config.GetConnectionString("Mongo");
-            if (connString = null)
+            if (connString == null)
             {
                 throw new Exception("Monogo DB connection string is missing.");
             }
             MongoClient client = new MongoClient(connString);
-            var collection
+            
 
         }
     }
