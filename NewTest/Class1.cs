3545541b853640859fe833e15e09e14d5439289a
Jacob <maimaisatt@gmail.com>
2023-09-18 09:03:37 +0800

    tesg

---
+++ NewTest/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace NewTest
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            ApiDataResponseModel mode = new ApiDataResponseModel();
+            MongoClient mongoClient = new MongoClient();
+    
+
+        }
+    }
+}
+
